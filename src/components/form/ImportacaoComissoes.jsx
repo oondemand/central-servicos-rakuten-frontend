0f81cb41712955eab7ddca13b53fa0c029f9ef61
Maike <maikonalexandre574@gmail.com>
2024-11-06 09:53:54 -0300

    feat:  create import commissions simple flow

--- src/components/form/ImportacaoComissoes.jsx
+++ src/components/form/ImportacaoComissoes.jsx
@@ -1,6 +1,31 @@
-import { Button } from "@chakra-ui/react";
+import React, {useRef} from 'react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Button } from '@chakra-ui/react';
+
+import { Input } from '@chakra-ui/react';
+
+import { useToast } from "@chakra-ui/react";
+
+import {importarComissoes } from "../../services/acoesEtapaService"
+
 
 const ImportacaoComissoes = () => {
+  const toast = useToast();
+  const inputFileRef = useRef(null);
+
+  const importCommissions = async(e) => {
+    await importarComissoes(e.target.files[0])
+   
+    toast({
+      title: "Arquivo importado com sucesso! Verifique seu email!",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+      position: "top-right"
+    });
+
+    inputFileRef.current.value = null;
+  }
+
   return (
     <div>
       <div>
@@ -8,16 +33,16 @@ const ImportacaoComissoes = () => {
           Selecione a Planilha de <i>Payment Control</i>:
         </label>
 
-        <div
-          style={{
-            display: "block",
-            margin: "50px 0",
-          }}
-        >
-          <input type="file" />
-        </div>
+        <div />
       </div>
-      <Button colorScheme="brand">Enviar</Button>
+        <Button mt={2} onClick={() => inputFileRef.current.click()} colorScheme="brand">Importar comissões</Button>
+        <Input
+          type="file"
+          ref={inputFileRef}
+          onChange={importCommissions}
+          style={{ display: "none" }}
+          accept=".xlsx, .xls, .xlsm, .xltx, .xltm, .xlsb"
+        />
     </div>
   );
 };
