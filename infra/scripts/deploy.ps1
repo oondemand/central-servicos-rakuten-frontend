788e7fbe5d4bed54bc5a161bde9a49c5a3dabd93
Fabio <90531115+fanaia@users.noreply.github.com>
2024-11-12 18:38:33 -0300

    update frontend image version to 1.0.0 and modify footer text

--- infra/scripts/deploy.ps1
+++ infra/scripts/deploy.ps1
@@ -1,5 +1,5 @@
 # Definir a versão da imagem, nome da imagem e o URL do registro de container
-$VERSION = "0.3.1"
+$VERSION = "1.0.0"
 $IMAGE_NAME = "cst-rakuten-frontend"
 $REGISTRY_URL = "registry.digitalocean.com/oondemand"
 
