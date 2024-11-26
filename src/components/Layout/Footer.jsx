1ba199cd4990476ff127b2de4c311d60c22b5b33
Fabio <90531115+fanaia@users.noreply.github.com>
2024-11-26 16:03:04 -0300

    update: bump frontend image version to 1.0.7 and update footer version display; adjust mask in PrestadorForm

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -5,7 +5,7 @@ import { Box, Text } from "@chakra-ui/react";
 const Footer = () => {
   return (
     <Box p={4} textAlign="center" bg="gray.50" color="gray.800">
-      <Text>&copy; Central de Serviços OonDemand vs 1.0.6</Text>
+      <Text>&copy; Central de Serviços OonDemand vs 1.0.7</Text>
     </Box>
   );
 };
